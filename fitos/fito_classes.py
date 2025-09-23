b893cb9f174fc5e7210252c3bfc56080a60a2906
moi <machinelearning001@meras.com.es>
2025-09-23 09:35:15 +0100

    2025-09-23_001_MyEarlyStoppingCallback

---
+++ fitos/fito_classes.py
@@ -0,0 +1,28 @@
+import transformers
+
+class MyEarlyStoppingCallback(transformers.TrainerCallback):
+    def __init__(self, metric_name,early_stopping_patience=5,lower_is_better=True):
+        self.metric_name = metric_name
+        self.early_stopping_patience = early_stopping_patience
+        self.lower_is_better = lower_is_better
+        self.best_metric = None
+        self.patience_counter = 0
+
+    def on_evaluate(self, args, state, control, metrics=None, **kwargs):
+        if metrics is None:
+            return
+
+        current_metric = metrics.get(self.metric_name)
+
+        if self.best_metric is None:
+            self.best_metric = current_metric
+        elif (self.lower_is_better & current_metric < self.best_metric)|(~self.lower_is_better & current_metric > self.best_metric):
+            self.best_metric = current_metric
+            self.patience_counter = 0
+        else:
+            self.patience_counter += 1
+            print(f"No improvement for {self.patience_counter} evaluations")
+
+            if self.patience_counter >= self.early_stopping_patience:
+                print("Early stopping triggered!")
+                control.should_training_stop = True
